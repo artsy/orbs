8305f9a3dcbfb4d33871f3ede5c79def7f4ddaee
Justin Bennett <zephraph@gmail.com>
2019-01-07 02:26:58 -0500

    Overhaul orb publishing

--- scripts/validate_orbs.sh
+++ scripts/validate_orbs.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-set -o pipefail
+set -euo pipefail
 
-VERSION_REGEX="[0-9]\.[0-9]\.[0-9]"
-
-for orb in src/**/*.yml; do 
-  version_comment=$(head -n 1 $orb)
-  version=$(echo $version_comment | grep -o "$VERSION_REGEX")
-
-  if [ -z $version ] || [ ! "${version_comment:0:1}" == "#" ]; then
-    echo "Every orb is expected to have a version comment on line 1"
-    echo "It should look something like '# Orb Version 1.0.0'"
-    exit 1
-  fi
-
-  circleci orb validate $orb 
+for orb in $(ls ./src); do
+  ./scripts/validate_orb.sh $orb
 done
