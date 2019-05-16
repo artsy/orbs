378328e1a4c8b64325a20c0b70da63801c0df747
Zephraph <zephraph@gmail.com>
2019-05-16 13:55:51 -0400

    Ensure new orbs can be published

--- scripts/publish_orb.sh
+++ scripts/publish_orb.sh
@@ -18,28 +18,34 @@ ORB="$1"
 
 ORB_PATH=$(get_orb_path $ORB)
 VERSION=$(get_orb_version $ORB)
-LAST_PUBLISHED=$(get_published_orb_version $ORB)
+IS_PUBLISHED=$(is_orb_published $ORB)
 
-case $(compare_version $VERSION $LAST_PUBLISHED) in
-  "=")
-    echo "artsy/$ORB@$VERSION is the latest, skipping publish"
-    exit 0
-    ;;
-  "<")
-    echo "artsy/$ORB@$LAST_PUBLISHED is the latest, cannot publish older version $VERSION"
-    echo "Please update $ORB_PATH to have a version greater than $LAST_PUBLISHED"
-    exit 1
-    ;;
-  ">")
-    echo "Preparing to bump artsy/$ORB from $LAST_PUBLISHED to $VERSION"
-    ;;
-  *)
-    echo "Version comparison for artsy/$ORB failed."
-    echo "Current version: $VERSION"
-    echo "Published version: $LAST_PUBLISHED"
-    exit 1
-    ;;
-esac
+if [ -z "$IS_PUBLISHED" ]; then
+
+  LAST_PUBLISHED=$(get_published_orb_version $ORB)
+
+  case $(compare_version $VERSION $LAST_PUBLISHED) in
+    "=")
+      echo "artsy/$ORB@$VERSION is the latest, skipping publish"
+      exit 0
+      ;;
+    "<")
+      echo "artsy/$ORB@$LAST_PUBLISHED is the latest, cannot publish older version $VERSION"
+      echo "Please update $ORB_PATH to have a version greater than $LAST_PUBLISHED"
+      exit 1
+      ;;
+    ">")
+      echo "Preparing to bump artsy/$ORB from $LAST_PUBLISHED to $VERSION"
+      ;;
+    *)
+      echo "Version comparison for artsy/$ORB failed."
+      echo "Current version: $VERSION"
+      echo "Published version: $LAST_PUBLISHED"
+      exit 1
+      ;;
+  esac
+
+fi
 
 circleci orb publish $ORB_PATH artsy/$ORB@$VERSION $TOKEN
 
