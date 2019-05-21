78d50d9b91bf55e55cd2e12428b58966ec921a54
Zephraph <zephraph@gmail.com>
2019-05-21 14:55:55 -0400

    Add canary deployment to orbs

---
+++ scripts/colors.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+COLORS_ENABLED=$([ -x "$(command -v tput)" ] && echo "true")
+
+_red=`tput setaf 1`
+_red=`tput setaf 2`
+_yellow=`tput setaf 3`
+_reset=`tput sgr0`
+
+COLOR() {
+  [ ! -z COLORS_ENABLED ] && echo "$1$2${_reset}" || echo "$2"
+}
+
+RED() {
+  echo $(COLOR $_red "$1")
+}
+
+GREEN() {
+  echo $(COLOR $_green "$1")
+}
+
+YELLOW() {
+  echo $(COLOR $_yellow "$1")
+}
